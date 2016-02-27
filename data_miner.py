121c72e9215f87586e62ce589868f91a7c281426
vivekhebbar <vhebbar13@gmail.com>
2016-02-27 13:38:11 -0800

    cleaned data_miner up a bit, will start working on training weights

--- data_miner.py
+++ data_miner.py
@@ -11,31 +11,32 @@ import numpy as np
 
 all_matrices = []
 
-for period in range(1,122):
-    r = requests.get('http://games.espn.go.com/fba/leaders?&scoringPeriodId=' + str(period) + '&seasonId=2016')
-    soup = BeautifulSoup(r.text)
-    t_r = soup.find("tr", attrs={"class":"playerTableBgRowSubhead tableSubHead"})
-    rows = [t.get_text() for t in t_r.find_all("td")]
-    print(rows)
-    stats = ['PLAYER', '', 'OPP', 'STATUS', '', 'MIN', 'FGM/FGA', 'FG%', 'FTM/FTA', 'FT%', '3PM', 'REB', 'AST', 'STL', 'BLK', 'PTS']
-    table = soup.find("table", attrs={"class":"playerTableTable tableBody"})
-    game_data = {}
-    player = ""
-    index = 0
-    real_index = 0
-    for i in table.find_all("td"):
-        if index > 1:
-            ind = real_index % len(rows) 
-            if ind == 0:
-                print(str(real_index) + " " + i.get_text())
-                player = i.get_text().split(",")[0].lower()
-                if "*" in player:
-                    player = player[:len(player)-1]
-                game_data[player] = {}
-            else:
-                game_data[player][stats[ind]] = i.get_text()
-            real_index += 1
-        index += 1
+def populateTrainingData():
+    for period in range(1,122):
+        r = requests.get('http://games.espn.go.com/fba/leaders?&scoringPeriodId=' + str(period) + '&seasonId=2016')
+        soup = BeautifulSoup(r.text)
+        t_r = soup.find("tr", attrs={"class":"playerTableBgRowSubhead tableSubHead"})
+        rows = [t.get_text() for t in t_r.find_all("td")]
+        print(rows)
+        stats = ['PLAYER', '', 'OPP', 'STATUS', '', 'MIN', 'FGM/FGA', 'FG%', 'FTM/FTA', 'FT%', '3PM', 'REB', 'AST', 'STL', 'BLK', 'PTS']
+        table = soup.find("table", attrs={"class":"playerTableTable tableBody"})
+        game_data = {}
+        player = ""
+        index = 0
+        real_index = 0
+        for i in table.find_all("td"):
+            if index > 1:
+                ind = real_index % len(rows) 
+                if ind == 0:
+                    print(str(real_index) + " " + i.get_text())
+                    player = i.get_text().split(",")[0].lower()
+                    if "*" in player:
+                        player = player[:len(player)-1]
+                    game_data[player] = {}
+                else:
+                    game_data[player][stats[ind]] = i.get_text()
+                real_index += 1
+            index += 1
 
     def createDataMatrix():
         # Creates Matrix A in Ax = B s.t. x is the weight vector,
@@ -50,5 +51,7 @@ for period in range(1,122):
                     big_matrix.append(vector)
         return big_matrix
 
-    if createDataMatrix():
-        all_matrices.append(createDataMatrix())
+    player_date_matrix = createDataMatrix()
+    if player_date_matrix:
+        all_matrices.append(player_date_matrix)
+
